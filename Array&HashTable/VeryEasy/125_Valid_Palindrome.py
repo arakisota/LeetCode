8fdb66987e894d09485025b00b1bdf3c83377792
arakisota <soutaraki0126@gmail.com>
2023-06-19 02:15:45 +0900

    125の問題を解答

---
+++ Array&HashTable/VeryEasy/125_Valid_Palindrome.py
@@ -0,0 +1,14 @@
+from string import ascii_lowercase
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        num = set([str(i) for i in range(10)])
+        num = num | set(ascii_lowercase)
+        new_s = s.lower()
+        for feature in list(new_s):
+            if feature not in num:
+                new_s = new_s.replace(feature, "")
+        if new_s == new_s[::-1]:
+            return True
+        else:
+            return False
