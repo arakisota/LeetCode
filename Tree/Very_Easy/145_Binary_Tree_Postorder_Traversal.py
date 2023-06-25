e93d0afe263efc27499c1d55dcccea865e6b8692
arakisota <soutaraki0126@gmail.com>
2023-06-25 17:43:35 +0900

    Preorder Traversal

---
+++ Tree/Very_Easy/145_Binary_Tree_Postorder_Traversal.py
@@ -0,0 +1,24 @@
+from typing import List, Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        sol = []
+        bag = [root]
+        while bag:
+            node = bag.pop()
+            sol.append(node.val)
+            if node.left:
+                bag.append(node.left)
+            if node.right:
+                bag.append(node.right)
+
+        return sol[::-1]
